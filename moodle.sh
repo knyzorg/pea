64ee4eab4ad4968475ecf23c3615650e81d51cf8
Slava Knyazev <slava@knyazev.io>
2020-02-07 12:37:57 -0500

    clean up after generation

--- moodle.sh
+++ moodle.sh
@@ -6,12 +6,14 @@ cp -r public moodle
 cd moodle
 for folder in `find . -mindepth 1 -type d`
 do
-cd $folder
-cp index.html ../$folder.html
-sed -i 's/\/\"/\.html"/g' ../$folder.html
-sed -i 's/"\.\.\//"\.\//g' ../$folder.html
-cd ..
-rm -rf $folder
+if [ -f $folder/index.html ]; then
+  cd $folder
+  cp index.html ../$folder.html
+  sed -i 's/\/\"/\.html"/g' ../$folder.html
+  sed -i 's/"\.\.\//"\.\//g' ../$folder.html
+  cd ..
+  rm -rf $folder
+fi
 done
 
 sed -i 's/\/\"/\.html"/g' index.html
